06007d9baedeebd152056fc8f4b5577dff91b857
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2021-10-10 12:41:31 -0300

    Testing file upload with multer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+  const fileElement = useRef(null);
+
+  const sendFile = async () => {
+    const dataForm = new FormData();
+    for (const file of fileElement.current.files) {
+      dataForm.append('file', file);
+    }
+    const res = await fetch(`http://localhost:8080/upload`, {
+      method: 'POST',
+      body: dataForm,
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" ref={fileElement} />
+      <button onClick={sendFile}>Send file</button>
     </div>
   );
 }
